6c1b16251696193d2ddba89642065f9931fe317e
beni-gut <beni.r.gut@gmail.com>
2021-07-23 00:09:36 +0200

    added console logs of failed QE-SparqlExecuter runs

--- scripts/start_pipeline.sh
+++ scripts/start_pipeline.sh
@@ -2,3 +2,4 @@
 
 cd /home/andokai/bachelorarbeit/Qanary/qanary_pipeline-template/target/
 java -jar /home/andokai/bachelorarbeit/Qanary/qanary_pipeline-template/target/qa.pipeline-2.4.0.jar --qanary.triplestore=http://admin:admin@localhost:5820/qanary
+
