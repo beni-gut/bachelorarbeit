01b23e748f217e36d021a2e8ad6fb9acfa8e39e1
beni-gut <beni-gut@users.noreply.github.com>
2021-08-12 10:18:11 +0200

    added some sparql for BA

--- scripts/dokumentation_files/installation_and_setup.sh
+++ scripts/dokumentation_files/installation_and_setup.sh
@@ -44,3 +44,49 @@ java -jar qanary_component-REL-RELNLIOD-2.0.0.jar
 DBpedia: "SELECT DISTINCT ?uri WHERE {<http://dbpedia.org/resource/Switzerland>   <http://dbpedia.org/ontology/capital> ?uri. }"
 
 SINA: "SELECT * WHERE {?uri <http://dbpedia.org/resource/Switzerland>   <http://dbpedia.org/ontology/capital>. }"
+
+
+# SPARQL Query from QALD-9, impossible by QB?
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX dbp: <http://dbpedia.org/property/>
+PREFIX dbr: <http://dbpedia.org/resource/>
+SELECT DISTINCT ?uri
+WHERE {
+  ?uri dbo:occupation dbr:Skateboarder {
+    ?uri dbo:birthPlace dbr:Sweden
+  } UNION {
+    ?uri dbo:birthPlace ?place .
+    ?place dbo:country dbr:Sweden
+  }
+}
+
+
+
+# SPARQL Query from QALD-9, impossible by QB?
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX dbp: <http://dbpedia.org/property/>
+PREFIX dbr: <http://dbpedia.org/resource/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX db: <http://dbpedia.org/>
+SELECT ?capacity
+WHERE {
+  {
+    dbr:FC_Porto dbo:ground ?ground .
+    ?ground dbo:capacity ?capacity
+  } UNION {
+    dbr:FC_Porto dbo:ground ?ground . ?ground dbp:capacity ?capacity
+  }
+}
+
+# SPARQL Query from QALD-9, impossible by QB?
+PREFIX dbo: <http://dbpedia.org/ontology/>
+SELECT DISTINCT ?uri
+WHERE {
+  <http://dbpedia.org/resource/Lovesick_(1983_film)> dbo:starring ?uri .
+  ?uri dbo:birthPlace ?city .
+  ?city dbo:country <http://dbpedia.org/resource/United_Kingdom>
+}
+
+
+
